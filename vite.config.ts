0d0fc91c4d546db494d3ed2ce586ef64c8d7af7c
Ethan O'Brien <146725124+EObrien60@users.noreply.github.com>
2025-11-30 10:53:15 +0000

    Configure base URL for GitHub Pages
    Set the base URL for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/QuantumHarbour.ie/', // This sets the base URL for GitHub Page
   plugins: [react()],
 })
